222cca7d08dfae80cadc6b7cfad386caada5d269
eryem-reyem <github@c-meyer.com>
2021-01-14 08:56:52 +0100

    New function computer player

--- flask_game.py
+++ flask_game.py
@@ -17,30 +17,58 @@ def index():
         session['user_id'] = str(uuid.uuid4())
     return render_template('game_index.html',  instruction='Play x in a row? Please ENTER!')
 
-@app.route("/game_start", methods=['POST'])
-def game_start():
-    board = all_boards[session['user_id']] = game.Board(5,5,3)
+@app.route("/game_start/<string:name>", methods=['POST'])
+def game_start(name):
+    board = all_boards[session['user_id']] = game.Board(7,6,4)
     board.turn_color = 'x'
+    if name == '1':
+        board.cpu = 0
+    print(board.cpu)
     return render_template('game_start.html', headline=board.player(), instruction='Please enter row!', 
     width=board.width, height=board.height, board=board.board[::-1], game_status=board.game_status)  
 
-@app.route("/game_player2", methods=['POST'])
+@app.route("/game_player2", methods=['POST', 'GET'])
 def game_player2():
     board = all_boards[session['user_id']]
-    ainput = request.form['name']
-    if int(ainput) <= board.width and board.lines[int(ainput)-1] < board.height:
-        board.drop(board.turn_color, int(ainput)-1)
+    if board.cpu == 0 or board.cpu == None:
+        ainput = request.form['name']
+        if int(ainput) <= board.width and board.lines[int(ainput)-1] < board.height:
+            board.drop(board.turn_color, int(ainput)-1)
+        try:
+            board.cpu += 1
+        except:
+            pass
+    elif board.cpu == 1:
+        if board.turn_color == 'o':
+            player = True
+        else:
+            player = False
+        board.computer(player)
+        board.cpu -= 1
     return render_template('game_player2.html',  headline=board.player(), instruction='Please enter row!',
-     width=board.width, height=board.height, board=board.board[::-1], game_status=board.game_status) 
+     width=board.width, height=board.height, board=board.board[::-1], game_status=board.game_status, cpu=board.cpu) 
 
         
 @app.route("/game_player1", methods=['POST'])
 def game_player1():
     board = all_boards[session['user_id']]
-    ainput = request.form['name']
-    board.drop(board.turn_color, int(ainput)-1)
+    if board.cpu == 0 or board.cpu == None:
+        ainput = request.form['name']
+        if int(ainput) <= board.width and board.lines[int(ainput)-1] < board.height:
+            board.drop(board.turn_color, int(ainput)-1)
+        try:
+            board.cpu += 1
+        except:
+            pass
+    elif board.cpu == 1:
+        if board.turn_color == 'o':
+            player = True
+        else:
+            player = False
+        board.computer(player)
+        board.cpu -= 1
     return render_template('game_start.html', headline=board.player(), instruction='Please enter row!',
-    width=board.width, height=board.height, board=board.board[::-1], game_status=board.game_status) 
+    width=board.width, height=board.height, board=board.board[::-1], game_status=board.game_status, cpu=board.cpu) 
 
 @app.route("/win")
 def win():
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
+
 class Board: 
     DIRECTIONS = ((1 ,0), (1, -1), (1, 1), (0, 1), (-1, 0), (-1, 1), (-1, -1), (0, -1))
-    def __init__(self, width, height, win=4):
+    def __init__(self, width, height, win=3):
         self.width = width
         self.height = height
         self.win = win
@@ -13,8 +15,120 @@ class Board:
         for line in range(self.width):
             self.lines.append(0)
         self.position = []
-        self.won_by = None
+        self.positions_index = defaultdict(list)
+        self.win_options = self.get_win_options()
+        self.cpu = None
+
+    def get_possible_wins(self, pos, direction):
+        possible_wins = set()
+        column, row = pos
+        d_column, d_row = direction
+        new_colmun, new_row = column+d_column*(self.win-1), row+d_row*(self.win-1)
         
+        if new_colmun < 0 or new_colmun >= self.width or new_row < 0 or new_row >= self.height:
+            return False
+        for i in range(self.win):
+            possible_wins.add((column+d_column*i, row+d_row*i))
+
+        return possible_wins
+
+    def get_win_options(self):
+        counter = 0
+        win_options = {}
+        known_possitions = set()
+        
+        for row in range(len(self.board)):
+            for column in range(len(self.board[row])):
+                for direction in self.DIRECTIONS:
+                    positions = self.get_possible_wins((column, row), direction)
+                    if not positions or positions in known_possitions:
+                        continue
+                    win_options[counter] = [0, 0] # player '0' / player 'x'
+                    for position in positions:
+                        self.positions_index[position].append(counter)
+                    known_possitions.add(frozenset(positions))
+                    counter += 1
+
+        return win_options
+
+    def set_token(self, pos, player):
+        x, y = pos
+        a_win = False
+        self.board[y][x] = 'o' if player else 'x'
+        for i in self.positions_index[pos]:
+            self.win_options[i][player] += 1
+            if self.win_options[i][player] == 4:
+                a_win = True
+        self.count_tokens += 1
+        self.lines[x] += 1
+
+        return a_win
+
+    def delete_token(self, pos, player):
+        x, y = pos
+        self.board[y][x] = '.'
+        for i in self.positions_index[pos]:
+            self.win_options[i][player] -= 1 
+        self.count_tokens -= 1
+        self.lines[x] -= 1
+
+    def rate(self):
+        score = 0
+        for row in range(len(self.board)):
+            for column in range(len(self.board[row])):
+                for i in self.positions_index[(column, row)]:
+                    player_2, player_1 = self.win_options[i]
+                    if player_1 > 0 and player_2 > 0: 
+                        continue
+                    score += player_1*10
+                    score -= player_2*10
+        return score
+
+    def list_of_moves(self):
+        moves = []
+        for column in range(self.width):
+            if self.lines[column] >= self.height:
+                continue
+            row = self.lines[column]
+            moves.append((column, row))
+        return moves
+
+    def computer(self, player):
+        rated_moves = []
+        for move in self.list_of_moves():
+            win = self.set_token(move, player)
+            score = self.minimax(5, -999999, 999999, player, win)
+            self.delete_token(move, player)
+            rated_moves.append((score, move))
+        rated_moves.sort(reverse=player)
+        score, best_move = rated_moves[0]
+        print(rated_moves)
+        print(best_move)
+        win = self.drop(self.turn_color, best_move[0])
+        return win
+
+    def minimax(self, cutoffs, alpha, beta, player, win):   # alpha beta pruning
+        if win:
+            return 99999+cutoffs if player else -99999-cutoffs
+        if cutoffs == 0 or self.count_tokens == (self.height*self.width):  # or spielfeld voll
+            return self.rate()
+        player = not player
+        value = -999999 if player else 999999
+        for move in self.list_of_moves():
+            #print(move, cutoffs)
+            win = self.set_token(move, player)
+            score = self.minimax(cutoffs-1, alpha, beta, player, win)
+            self.delete_token(move, player)
+            if player:
+                value = max(value, score)
+                alpha = max(value, alpha)
+            else:
+                value = min(value, score)
+                beta = min(value, beta)
+            if alpha >= beta:
+                break
+        return value      
+
     def ascii(self):
         out=['']
         for i in range(len(self.board)):
@@ -40,31 +154,35 @@ class Board:
         for i in self.axis_strings(self.position[0],self.position[1]):
             if self.turn_color * self.win in i:
                 self.game_status = 'over'
-                self.won_by = self.turn_color 
-                print(self.player(), '(', self.turn_color,')', ' winns!')
+                #print(self.ascii())
+                #print(self.player(), '(', self.turn_color,')', ' winns!')
 
     def drop(self, color, slot):
         if self.turn_color == None:
                 self.turn_color = color
         assert self.turn_color == color, 'Wrong Player'
         tile = Tile(color, slot, self.lines[slot])
-        try:           
-            self.board[tile.position[1]][tile.position[0]] = tile.color
-            self.lines[slot] += 1
-            self.position = tile.position[::-1]
-            self.check_game_status()
-            if self.game_status == 'active':
-                self.count_tokens += 1
+        if self.turn_color == 'o':
+            player = True
+        else:
+            player = False
+        try:      
+            #print((slot, self.lines[slot]))   
+            self.position = tile.position [::-1]  
+            self.set_token((slot, self.lines[slot]), player)
+            self.check_game_status() 
+            if self.game_status == 'active': 
                 if self.turn_color == 'x':
                     self.turn_color = 'o'
                 else:
                     self.turn_color = 'x'
+            
             return tile
         except IndexError:
             board.drop(board.turn_color, int(input('Out of range! Please, choose slot: ')) - 1)
 
     def player(self):
-        if self.count_tokens % 2 == 0:
+        if self.count_tokens % 2 == 1:
             return 'Player 1'
         else:
             return 'Player 2'
@@ -117,29 +235,38 @@ class Tile:
 
 
 if __name__ == '__main__':
-    '''
-    def trim_board(ascii_board):
-        return '\n'.join([i.strip() for i in ascii_board.splitlines()])
-    t = trim_board
-    board = Board.load(t("""
-    o..
-    o..
-    xxx
-    """))'''
-
-   
-   
-    board = Board(5,5)
+    board = Board(7,6,4)
 
     print('Starting Game!')
     print()
+
+    a = input('1=cpu   |     2=alone:   ')
+    print(a)
+    if a == '1':
+        board.cpu = 0
+
     board.choose_player()
 
     while board.game_status == 'active':
+
         print(board.player())
         print(board.ascii())
         
-        board.drop(board.turn_color, int(input('Please, choose slot: ')) - 1)
+        if board.cpu == 0 or board.cpu == None:
+            board.drop(board.turn_color, int(input('Please, choose slot: ')) - 1)
+            try:
+                board.cpu += 1
+            except:
+                continue
+        elif board.cpu == 1:
+            if board.turn_color == 'o':
+                player = True
+            else:
+                player = False
+            board.computer(player)
+            board.cpu -= 1
+
+        
         
     
         
